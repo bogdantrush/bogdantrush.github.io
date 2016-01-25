522f2af6593e92126913eb64d4915b77b7a0f835
bogdantrush <ibogdantrush@gmail.com>
2016-01-25 11:54:23 +0300

    Revert "ddd"
    This reverts commit 37192e0208b8e6646e1eb8d2fc48c9285a49191c.

--- js/another.js
+++
@@ -1,8 +0,0 @@
-function main(){
-	var sum = "";
-
-	
-		sum = intext.value.match(/[0-9]+[\+\-\/\*][0-9]+/);
-		intext.value = intext.value.replace(/[0-9]+[\+\-\/\*][0-9]+/,eval(sum.toString()));
-	};
-}
